261781755d5210cfa5ad922f0051575bb6c9dee8
nodev918 <devnode77@gmail.com>
2022-05-22 12:41:23 +0800

    hhh

--- src/operation/CRUD.go
+++ src/operation/CRUD.go
@@ -7,16 +7,26 @@ import (
 )
 
 func Run(){
-	fmt.Println("run")
 	
-	// a := []byte("hello world")
+	a := []byte("dddddd")
 
-	file,err := os.Create("test.txt"); if err != nil {
+	// b := fmt.Printf("%d",a)
+
+	// fmt.Println("b: ",b)
+
+	fmt.Println(a)
+
+	path , err := os.Getwd(); if err != nil {
+		log.Println(err)
+	}
+
+
+	file,err := os.OpenFile(path+"/data/test.txt",os.O_WRONLY|os.O_TRUNC|os.O_CREATE,0666); if err != nil {
 		log.Fatalf("failed creating file: %s", err)
 	}
 	defer file.Close()
 
-	len, err := file.WriteString("helloo"); if err != nil {
+	len, err := file.Write(a); if err != nil {
 		log.Fatalf("failed writing file: %s", err)
 	}
 	fmt.Printf("\n file name: %s", file.Name())
